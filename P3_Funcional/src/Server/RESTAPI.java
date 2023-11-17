f142b074048d8faeb10576c157675aef549feb22
JBB092 <jos.barquero@estudiantec.cr>
2023-11-17 07:56:23 -0600

    Documentación

--- P3_Funcional/src/Server/RESTAPI.java
+++ P3_Funcional/src/Server/RESTAPI.java
@@ -1,4 +1,5 @@
 package Server;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -12,18 +13,37 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Scanner;
 
-public class RESTAPI implements Runnable{
+/**
+ * RESTAPI class represents a simple HTTP server providing RESTful API functionality.
+ *
+ * <p>This class implements the Runnable interface to run the HTTP server in a separate thread.</p>
+ *
+ * <p>Author: Alejandro Solis</p>
+ */
+public class RESTAPI implements Runnable {
 
+    /**
+     * Constructor for the RESTAPI class.
+     * Creates a new thread to run the HTTP server.
+     */
     public RESTAPI() {
-        Thread escucho = new Thread(this);
-        escucho.start();
+        Thread listenerThread = new Thread(this);
+        listenerThread.start();
     }
+
+    /**
+     * Implementation of the run method from the Runnable interface.
+     * Starts the HTTP server and sets up the request handler.
+     */
     @Override
     public void run() {
         try {
-            HttpServer apiServer = HttpServer.create(new InetSocketAddress(9999),128);
-            System.out.println("Servidor abierto");
-            apiServer.createContext("/",new SimpleHandler());
+            // Create HTTP server at port 9999
+            HttpServer apiServer = HttpServer.create(new InetSocketAddress(9999), 128);
+            System.out.println("Server opened");
+
+            // Set up the context and request handler
+            apiServer.createContext("/", new SimpleHandler());
             apiServer.setExecutor(null);
             apiServer.start();
         } catch (IOException e) {
@@ -31,37 +51,63 @@ public class RESTAPI implements Runnable{
         }
     }
 }
-class SimpleHandler implements HttpHandler
 
-{
-    private JSONObject getJsonBody(InputStream body){
-        Scanner s = new Scanner(body).useDelimiter("\\A");
-        String jsonImput = s.hasNext() ? s.next() : "";
-        s.close();
+/**
+ * SimpleHandler class implements the HttpHandler interface to handle HTTP requests.
+ *
+ * <p>This class is responsible for parsing JSON requests and generating appropriate responses.</p>
+ *
+ * <p>Author: Alejandro Solis</p>
+ */
+class SimpleHandler implements HttpHandler {
+
+    /**
+     * Parses the JSON body from the input stream.
+     *
+     * @param body The input stream containing the JSON data.
+     * @return A JSONObject representing the parsed JSON data.
+     */
+    private JSONObject getJsonBody(InputStream body) {
+        Scanner scanner = new Scanner(body).useDelimiter("\\A");
+        String jsonInput = scanner.hasNext() ? scanner.next() : "";
+        scanner.close();
         JSONParser parser = new JSONParser();
         JSONObject jsonObject;
         try {
-            jsonObject = (JSONObject) parser.parse(jsonImput);
+            jsonObject = (JSONObject) parser.parse(jsonInput);
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
         return jsonObject;
     }
+
+    /**
+     * Handles POST requests and generates a response based on the JSON data received.
+     *
+     * @param myJson The JSON data received in the POST request.
+     * @return A JSONObject representing the response to the POST request.
+     * @throws InterruptedException If the handling process is interrupted.
+     */
     private JSONObject handlePOST(JSONObject myJson) throws InterruptedException {
-        System.out.println("handle metodo aceptado");
-        JSONObject respuesta = new JSONObject();
-        String metodo = (String) myJson.get("tipoConsulta");
-        if(metodo.equals("login")){
-            //implement
+        System.out.println("handle method accepted");
+        JSONObject response = new JSONObject();
+        String method = (String) myJson.get("tipoConsulta");
+        if (method.equals("login")) {
+            // Implementation for login
+        } else if (method.equals("register")) {
+            // Implementation for registration
         }
-        else if(metodo.equals("register")){
-            //IMPLEMENT
-        }
-        return respuesta;
+        return response;
     }
+
+    /**
+     * Handles HTTP requests.
+     *
+     * @param exchange The HttpExchange object representing the HTTP request and response.
+     * @throws IOException If an I/O error occurs during request handling.
+     */
     @Override
-    public void handle(HttpExchange exchange) throws IOException
-    {
+    public void handle(HttpExchange exchange) throws IOException {
         System.out.println("handle");
         if ("POST".equals(exchange.getRequestMethod())) {
             System.out.println("post");
@@ -75,19 +121,16 @@ class SimpleHandler implements HttpHandler
                 throw new RuntimeException(e);
             }
 
+            // Send the response to the client
             exchange.sendResponseHeaders(200, responseJson.toJSONString().getBytes().length);
-
-
             OutputStream output = exchange.getResponseBody();
             output.write(responseJson.toJSONString().getBytes());
             output.flush();
-
-
         } else if (("GET".equals(exchange.getRequestMethod()))) {
-            //METODOS PARA GET
+            // Methods for handling GET requests
         } else {
-            //CUALQUIER OTRO METODO NO SE CONTEMPLA A MENOS DE QUE SE ESPECIFIQUE
-            exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            // Any other method is not allowed unless specified
+            exchange.sendResponseHeaders(405, -1); // 405 Method Not Allowed
         }
         exchange.close();
     }
--- P3_Funcional/src/Server/RestClient.java
+++ P3_Funcional/src/Server/RestClient.java
@@ -11,31 +11,44 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * RestClient class represents an asynchronous task for making HTTP POST requests to a server.
+ *
+ * <p>This class extends AsyncTask and is used to communicate with a server and handle its response.</p>
+ *
+ * <p>Author: Alejandro Solis</p>
+ */
 public class RestClient extends AsyncTask<Void, Void, String> {
 
-    private static final String SERVER_URL = "http://tu-direccion-ip:9999/"; // Reemplaza con la dirección IP del servidor
+    private static final String SERVER_URL = "http://your-ip-address:9999/"; // Replace with the server's IP address
 
+    /**
+     * Performs the background task of making a POST request to the server.
+     *
+     * @param params No parameters are passed to this method.
+     * @return A string representing the server's response.
+     */
     @Override
     protected String doInBackground(Void... params) {
         try {
-            // Conecta al servidor
+            // Connect to the server
             URL url = new URL(SERVER_URL);
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("POST");
             urlConnection.setRequestProperty("Content-Type", "application/json");
             urlConnection.setDoOutput(true);
 
-            // Construye el JSON para la solicitud
+            // Build the JSON for the request
             JSONObject jsonParam = new JSONObject();
-            jsonParam.put("tipoConsulta", "login"); // Reemplaza con tus datos
+            jsonParam.put("tipoConsulta", "login"); // Replace with your data
 
-            // Envía el JSON al servidor
+            // Send the JSON to the server
             DataOutputStream outputStream = new DataOutputStream(urlConnection.getOutputStream());
             outputStream.writeBytes(jsonParam.toString());
             outputStream.flush();
             outputStream.close();
 
-            // Lee la respuesta del servidor
+            // Read the server's response
             InputStream in = urlConnection.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             StringBuilder result = new StringBuilder();
@@ -44,30 +57,35 @@ public class RestClient extends AsyncTask<Void, Void, String> {
                 result.append(line);
             }
 
-            // Cierra la conexión
+            // Close the connection
             in.close();
             urlConnection.disconnect();
 
-            // Devuelve la respuesta del servidor
+            // Return the server's response
             return result.toString();
 
         } catch (IOException e) {
-            Log.e("RestClient", "Error al conectarse al servidor", e);
+            Log.e("RestClient", "Error connecting to the server", e);
             return null;
         } catch (Exception e) {
-            Log.e("RestClient", "Error en la solicitud HTTP", e);
+            Log.e("RestClient", "Error in HTTP request", e);
             return null;
         }
     }
 
+    /**
+     * Handles the server's response after the background task is completed.
+     *
+     * @param result The string representing the server's response.
+     */
     @Override
     protected void onPostExecute(String result) {
-        // Maneja la respuesta del servidor aquí
+        // Handle the server's response here
         if (result != null) {
-            Log.d("RestClient", "Respuesta del servidor: " + result);
-            // Puedes procesar la respuesta JSON aquí
+            Log.d("RestClient", "Server response: " + result);
+            // Process the JSON response here
         } else {
-            Log.e("RestClient", "Error en la solicitud HTTP");
+            Log.e("RestClient", "Error in HTTP request");
         }
     }
 }
